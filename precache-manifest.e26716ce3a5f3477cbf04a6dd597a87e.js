be9a084091dd631e6dc57cadd6933120488e3146
Marrick Somal <marrick.somal@gmail.com>
2021-05-08 11:52:38 +0100

    Updates

---
+++ precache-manifest.e26716ce3a5f3477cbf04a6dd597a87e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe58581ef3636abdc12a9c63f31bed1a",
+    "url": "/robotcards/index.html"
+  },
+  {
+    "revision": "1495b50450f70d2f71c5",
+    "url": "/robotcards/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0eae8a8249933d9d82b6",
+    "url": "/robotcards/static/css/main.12525fc0.chunk.css"
+  },
+  {
+    "revision": "1495b50450f70d2f71c5",
+    "url": "/robotcards/static/js/2.53ec2793.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/robotcards/static/js/2.53ec2793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eae8a8249933d9d82b6",
+    "url": "/robotcards/static/js/main.84832639.chunk.js"
+  },
+  {
+    "revision": "ba2c9cb44bd4bed27b1c",
+    "url": "/robotcards/static/js/runtime-main.823e8af7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotcards/static/media/SEGA.12a3db8b.woff"
+  }
+]);
